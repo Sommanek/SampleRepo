142d14833e5ef6cee740f7cfb6016e283a1369d4
Sommanek <sommaneknimesh@gmail.com>
2014-09-06 17:13:59 +0530

    c

--- EHRProject/src/main/java/com/EHR/Controller/PatientController.java
+++ EHRProject/src/main/java/com/EHR/Controller/PatientController.java
@@ -1,5 +1,16 @@
 package com.EHR.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Patient")
+@Controller
 public class PatientController {
 
+	@RequestMapping("/addPatient")
+	public String addPatient(ModelMap map){
+		
+		return "AddPatient";
+	}
 }
